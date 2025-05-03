5276c9a17ca6f4627542a410f77d0fcebd65a3be
Abdallah Mahmoud <abdallah.momo132@gmail.com>
2025-05-03 10:13:31 +0300

    Implement routing with react-router-dom in App component
    - Integrated react-router-dom to enable navigation between SplashScreen, Home, and AboutPage components.
    - Wrapped the main application in a Router and defined routes for improved user experience and application structure.
    - Utilized MainLayout for consistent layout across different pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import AboutPage from "./pages/AboutPage";
+import SplashScreen from "./pages/SplashScreen";
 import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
+import MainLayout from "./layouts/MainLayout";
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<SplashScreen />} />
+          <Route
+            path="/home"
+            element={
+              <MainLayout>
+                <Home />
+              </MainLayout>
+            }
+          />
+          <Route
+            path="/about"
+            element={
+              <MainLayout>
+                <AboutPage />
+              </MainLayout>
+            }
+          />
+        </Routes>
+      </Router>
     </QueryClientProvider>
   );
 }
